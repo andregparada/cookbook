78a0a5872e16d9b7a8f16edfe46e3022b92ee946
André Parada <andregparada@gmail.com>
2025-02-10 16:48:45 -0300

    feat: prisma ingredientsOnDishes repository

---
+++ src/repositories/prisma/prisma-ingredients-on-dishes-repository.ts
@@ -0,0 +1,25 @@
+import { Prisma } from '@prisma/client'
+import { IngredientsOnDishesRepository } from '../ingredients-on-dishes-repository'
+import { prisma } from '@/lib/prisma'
+
+export class PrismaIngredientsOnDishesRepository
+  implements IngredientsOnDishesRepository
+{
+  async create(data: Prisma.IngredientsOnDishesUncheckedCreateInput) {
+    const ingredientsOnDish = await prisma.ingredientsOnDishes.create({
+      data,
+    })
+
+    return ingredientsOnDish
+  }
+
+  async findByDishId(dishId: string) {
+    const ingedientsOnDish = await prisma.ingredientsOnDishes.findMany({
+      where: {
+        dish_id: dishId,
+      },
+    })
+
+    return ingedientsOnDish
+  }
+}
