903a2e5a6d7067f1b7aa02f39b8dc6923fd419be
André Parada <andregparada@gmail.com>
2025-03-06 10:35:16 -0300

    feat: profile controller test

--- src/utils/test/create-and-authenticate-user.ts
+++ src/utils/test/create-and-authenticate-user.ts
@@ -2,22 +2,24 @@ import { prisma } from '@/lib/prisma'
 import { hash } from 'bcryptjs'
 import { FastifyInstance } from 'fastify'
 import request from 'supertest'
+import { createUserData } from './factories/user-data'
 
 export async function createAndAuthenticateUser(
   app: FastifyInstance,
   isAdmin = false,
 ) {
+  const userData = createUserData()
+
   await prisma.user.create({
     data: {
-      name: 'John Doe',
-      email: 'johndoe@example.com',
-      password_hash: await hash('123456', 6),
+      ...userData,
+      passwordHash: await hash('123456', 6),
       role: isAdmin ? 'ADMIN' : 'MEMBER',
     },
   })
 
   const authResponse = await request(app.server).post('/sessions').send({
-    email: 'johndoe@example.com',
+    email: userData.email,
     password: '123456',
   })
 
@@ -25,5 +27,6 @@ export async function createAndAuthenticateUser(
 
   return {
     token,
+    email: userData.email,
   }
 }
